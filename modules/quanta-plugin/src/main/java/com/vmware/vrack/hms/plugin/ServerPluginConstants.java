973c2785861828ef285d9e167f64d6473eb60787
Gautam Kumar <gautamk@vmware.com>
2016-07-04 18:29:22 +0530

    Added Quanta plugin, which uses IPMI interface to fetch hardware data through OOB.

---
+++ modules/quanta-plugin/src/main/java/com/vmware/vrack/hms/plugin/ServerPluginConstants.java
@@ -0,0 +1,10 @@
+package com.vmware.vrack.hms.plugin;
+
+public class ServerPluginConstants {
+    /* Board Name */
+    public static final String BOARD_NAME = "S210";
+
+    /* Board Manufacturer */
+    public static final String BOARD_MANUFACTURER = "Quanta";
+
+}
