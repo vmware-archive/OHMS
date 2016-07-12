b0c5ea3d81423f68753b187b178a80463f79f183
Gautam Kumar <gautamk@vmware.com>
2016-07-12 17:13:22 +0530

    Fixed the typo present in the license header template.

--- modules/hms-core/src/main/java/com/vmware/vrack/hms/task/ib/IBTask.java
+++ modules/hms-core/src/main/java/com/vmware/vrack/hms/task/ib/IBTask.java
@@ -9,8 +9,8 @@
 
  * Unless required by applicable law or agreed to in writing, software distributed
  * under the License is distributed on an "AS IS" BASIS, without warranties or
- * conditions of any kind, EITHER EXPRESS OR IMPLIED. see the License for the
- * specific language governing permissions and limitations under the License
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED. See the License for the
+ * specific language governing permissions and limitations under the License.
  *
  * *******************************************************************************/
 package com.vmware.vrack.hms.task.ib;
